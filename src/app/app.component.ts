c56be176e44910f58944ff7fb13c0e9cecac2b82
Adhikari <bishrant.adhikari@tfs.tamu.edu>
2020-02-10 16:55:43 -0600

    added a new branch for spatial selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fev';
+  title = 'Forest Ecosystem Values';
+  sidenavOpened = true;
+  constructor(private http: HttpClient) {}
+  fileUpload = (file) => {
+
+    const form = new FormData();
+    const publishParams: any = {
+        targetSR: {
+            wkid: 102100
+        },
+        maxRecordCount: 1000,
+        enforceInputFileSizeLimit: true,
+        enforceOutputJsonSizeLimit: true
+    };
+    publishParams.generalize = false;
+    publishParams.reducePrecision = false;
+    publishParams.numberOfDigitsAfterDecimal = 0;
+    form.append('publishParameters', JSON.stringify(publishParams));
+    form.append('filetype', 'shapefile');
+    form.append('f', 'json');
+    form.append('file', file);
+    this.http.post('https://www.arcgis.com/sharing/rest/content/features/generate', form).subscribe((ff) => {
+      console.log(ff);
+    })
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import './config';
+// import "@dojo/shim/Promise";
 
 if (environment.production) {
   enableProdMode();
